4b8b3b9a7b68b2067451f7f31ddf84458496e097
Didymus <didymus.benson@gmail.com>
2017-09-22 11:17:33 -0600

    hr

--- script.js
+++ script.js
@@ -49,9 +49,9 @@ function addTinyMCE() {
         plugins: [
                     'advlist autolink lists link image charmap print preview anchor textcolor',
                     'searchreplace visualblocks code fullscreen',
-                    'insertdatetime media table contextmenu paste table code help'
+                    'insertdatetime media table contextmenu paste table hr code help'
                 ],
-        toolbar: 'insert | undo redo |  styleselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | code | table | help',
+        toolbar: 'insert | undo redo |  styleselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | code | table | hr | help',
         convert_urls: false,
         init_instance_callback: function (editor) {
             editor.on('KeyUp', function (e) {
